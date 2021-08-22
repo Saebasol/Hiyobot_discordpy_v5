c72f411fd467b786c103b03cabb52f1dcb5ee125
green1052 <cydyellowgreen@gmail.com>
2021-08-22 23:45:27 +0900

    1

---
+++ hiyobot/cogs/nsfw/heliotrope.py
@@ -0,0 +1,76 @@
+from discord import Embed
+from discord.ext import commands
+
+from hiyobot.bot import Hiyobot
+from utils.paginator import Paginator
+
+
+class Heliotrope(commands.Cog):
+    def __init__(self, bot: Hiyobot):
+        self.bot = bot
+
+    @commands.command("번호")
+    @commands.is_nsfw()
+    async def _info(self, ctx: commands.Context, index: int):
+        """
+        작품 번호를 입력하면 히토미에서 해당 작품 정보를 가져옵니다.
+        사용할 수 있는 값 : 작품 번호(필수)
+        사용 예시 : ``&번호 1867978``
+        """
+
+        message = await ctx.send(embed=Embed(title="정보를 요청합니다. 잠시만 기다려주세요."))
+
+        if embed := await self.bot.mintchoco.gallery_info(index):
+            return await message.edit(embed=embed)
+
+        await message.edit(embed=Embed(title="정보를 찾지 못했습니다."))
+
+    @commands.command("검색")
+    @commands.is_nsfw()
+    async def _search(self, ctx: commands.Context, *, query: str):
+        """
+        검색을 요청합니다.
+        """
+
+        message = await ctx.send(embed=Embed(title="정보를 요청합니다. 잠시만 기다려주세요."))
+
+        if embeds := await self.bot.mintchoco.gallery_search(query.split(" ")):
+            return await message.edit(embed=embeds[0], view=Paginator(ctx.author.id, embeds))
+
+        await message.edit(embed=Embed(title="정보를 찾지 못했습니다."))
+
+    @commands.command(name="리스트")
+    @commands.is_nsfw()
+    async def _list(self, ctx: commands.Context, num: int = 1):
+        """
+        히토미에서 최근 올라온 한국어 작품을 가져옵니다.
+        사용할 수 있는 값 : 페이지(입력하지 않을 경우 1)
+        사용 예시 : ``&리스트`` 또는 ``&리스트 2``
+        """
+
+        message = await ctx.send(embed=Embed(title="정보를 요청합니다. 잠시만 기다려주세요"))
+
+        if embeds := await self.bot.mintchoco.gallery_list(num):
+            return await message.edit(embed=embeds[0], view=Paginator(ctx.author.id, embeds))
+
+        await message.edit(embed=Embed(title="정보를 찾지 못했습니다."))
+
+    @commands.command(name="뷰어")
+    @commands.is_nsfw()
+    async def _viewer(self, ctx: commands.Context, index: int):
+        """
+        작품 번호를 입력하면 디스코드 내에서 보여줍니다.
+        인자값: 작품 번호(필수)
+        사용법: ``&뷰어 1496588``
+        """
+
+        message = await ctx.send(embed=Embed(title="정보를 요청합니다. 잠시만 기다려주세요."))
+
+        if embeds := await self.bot.mintchoco.gallery_viewer(index):
+            return await message.edit(embed=embeds[0], view=Paginator(ctx.author.id, embeds))
+
+        await message.edit(embed=Embed(title="정보를 찾지 못했습니다."))
+
+
+def setup(bot: Hiyobot):
+    bot.add_cog(Heliotrope(bot))
--- utils/mintchoco.py
+++ utils/mintchoco.py
@@ -1,11 +1,141 @@
 # pyright: strict
 
+import re
+from typing import Iterable
+
 from aiohttp.client import ClientSession
+from discord import Embed
 from mintchoco.client import Client
+from mintchoco.model import HeliotropeInfo, Tag, HeliotropeImages
 
 
 class MintChoco(Client):
     client_session: ClientSession
 
+    def get_image_url(self, url: str):
+        url_parse_regex = re.compile(
+            r"\/\/(..?)(\.hitomi\.la|\.pximg\.net)\/(.+?)\/(.+)"
+        )
+        parsed_url: list[str] = url_parse_regex.findall(url)[0]
+
+        prefix = parsed_url[0]
+        main_url = parsed_url[1].replace(".", "_")
+        type_ = parsed_url[2]
+        image = parsed_url[3].replace("/", "_")
+        return f"{self.API_URL}/api/proxy/{prefix}_{type_}{main_url}_{image}"
+
+    @staticmethod
+    def parse_value_url(value_url_list: Iterable[Tag]):
+        if value_url_list:
+            return [
+                f"[{value_url_dict.value}](https://hitomi.la{value_url_dict.url})"
+                for value_url_dict in value_url_list
+            ]
+
+        return ["없음"]
+
     async def close(self):
         await self.client_session.close()
+
+    def make_embed_with_info(self, info: HeliotropeInfo):
+        tags_join = (
+            ", ".join(self.parse_value_url(info.tags))
+                .replace("♀", "\\♀")
+                .replace("♂", "\\♂")
+        )
+
+        embed = Embed(
+            title=info.title,
+            description=f"[{info.language.value}]({info.language.url})",
+            url=f"https://hitomi.la/galleries/{info.index}.html",
+        )
+
+        embed.set_thumbnail(url=self.get_image_url(info.thumbnail))
+
+        embed.add_field(
+            name="번호",
+            value=f"[{info.index}](https://hitomi.la/reader/{info.index}.html)",
+            inline=False,
+        )
+
+        embed.add_field(
+            name="타입",
+            value=f"[{info.type.value}]({info.type.url})",
+            inline=False,
+        )
+
+        embed.add_field(
+            name="작가", value=",".join(self.parse_value_url(info.artist)), inline=False
+        )
+
+        embed.add_field(
+            name="그룹", value=",".join(self.parse_value_url(info.group)), inline=False
+        )
+
+        embed.add_field(
+            name="원작", value=",".join(self.parse_value_url(info.series)), inline=False
+        )
+
+        embed.add_field(
+            name="캐릭터",
+            value=",".join(self.parse_value_url(info.characters)),
+            inline=False,
+        )
+
+        embed.add_field(
+            name="태그",
+            value=tags_join if len(tags_join) <= 1024 else "표시하기에는 너무 길어요.",
+            inline=False,
+        )
+
+        return embed
+
+    def make_viewer_embed(self, img_list: HeliotropeImages, total: int) -> list[Embed]:
+        embeds = []
+        num = 0
+
+        for file_info in img_list.files:
+            num += 1
+            embeds.append(
+                Embed()
+                .set_image(url=f"{self.API_URL}/api/proxy/{file_info.url}")
+                .set_footer(text=f"{num}/{total} 페이지")
+            )
+
+        return embeds
+
+    async def gallery_info(self, index: int):
+        search_info = await self.info(index)
+
+        return (
+            self.make_embed_with_info(search_info)
+            if search_info.status == 200 else
+            None
+        )
+
+    async def gallery_search(self, query: list[str]):
+        search_info = await self.search(query)
+
+        return (
+            [self.make_embed_with_info(result) for result in search_info.result]
+            if search_info.status == 200 else
+            None
+        )
+
+    async def gallery_list(self, number: int):
+        search_info = await self.list(number)
+
+        return (
+            [self.make_embed_with_info(result) for result in search_info.list]
+            if search_info.status == 200 else
+            None
+        )
+
+    async def gallery_viewer(self, index: int):
+        search_info = await self.galleryinfo(index)
+
+        return (
+            self.make_viewer_embed(await self.images(index), len(list(search_info.files)))
+            if search_info.status == 200 else
+            None
+        )
